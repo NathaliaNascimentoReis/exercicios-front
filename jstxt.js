d61e6b49157a50ef1f330fcf44266e053cba4245
Nath <nathalia.n.reis@aluno.senai.br>
2026-02-09 09:27:30 -0300

    feat: finaliza exercícios

--- jstxt.js
+++ jstxt.js
@@ -1,36 +1,71 @@
 // Transforme as funções tradicionais abaixo em arrow functions de uma única linha.
 
-// 1- Dobrar um número
+// 1. Dobrar um número
 function dobrar(n) {
-    return n * 2;
+  return n * 2;
 }
 
 const dobrar = (n) => n * 2;
 
 // 2. Criar uma saudação
 function saudar(nome) {
-    return "Olá, " + nome + "!";
+  return "Olá, " + nome + "!";
 }
 
-const saudar = nome => "Olá" + nome + "!";
+const saudar = (nome) => "Olá" + nome + "!";
 
-// 3- Somar números
+// 3. Somar números
 function somar(a, b) {
-    return (a + b);
+  return a + b;
 }
 
 const somar = (a, b) => a + b;
 
-// Verificar se é par
+// 4. Verificar se é par
 function ehPar(n) {
-    return n % 2 === 0;
+  return n % 2 === 0;
 }
 
-const ehPar = n => n % 2 === 0;
+const ehPar = (n) => n % 2 === 0;
 
 // 5. Converter Celsius para Fahrenheit
 function converter(celsius) {
-    return (celsius * 9/5) + 32;
+  return (celsius * 9) / 5 + 32;
 }
 
-const converter = celsius => (celsius * 9/5) + 32;
+const converter = (celsius) => (celsius * 9) / 5 + 32;
+
+// 6. Função que recebe um número e retorna o valor dele multiplicado por 10
+function multiDez(n) {
+  return n * 10;
+}
+
+const multiplicarPorDez = (n) => m * 10;
+
+// 7. Função que recebe a idade em anos e retorna a idade convertida em dias
+function idadeEmDias(n) {
+  return n * 365;
+}
+
+const idadeEmDias = (n) => n * 365;
+
+// 8. Converter letras para Maiúsculo
+function gritar(string) {
+  return string.toUpperCase();
+}
+
+const gritar = (texto) => texto.toUpperCase();
+
+// 9. Desconto de 5%
+function aplicarDesconto(n) {
+  return n * 0.05;
+}
+
+const desconto = (n) => n * 0.05;
+
+// 10. Subtração Simples
+function somar(a, b) {
+  return a - b;
+}
+
+const somar = (a, b) => a - b;
